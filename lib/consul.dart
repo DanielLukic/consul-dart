b3d80414221e0e87e52a46e333d97bc69ae6ec2e
Daniel Lukic <daniel.lukic@gmx.net>
2024-04-05 11:18:23 +0200

    Update primary library entry point to export what is needed

--- lib/consul.dart
+++ lib/consul.dart
@@ -1,1 +1,8 @@
+export 'package:ansi/ansi.dart';
+export 'package:console/console.dart';
+export 'package:consul/src/auto_help.dart';
+export 'package:consul/src/block_canvas.dart';
+export 'package:consul/src/consul/con_io/mad_con_io.dart';
 export 'package:consul/src/consul/desktop.dart';
+export 'package:consul/src/debug_log.dart';
+export 'package:consul/src/util/auto_dispose.dart';
