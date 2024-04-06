2600cdc9f7c31fed33fb084dbbdcba3122f4123e
Daniel Lukic <daniel.lukic@gmx.net>
2024-04-06 19:00:47 +0200

    Add CompositeDisposable and extract required shared functionality

--- lib/src/util/auto_dispose.dart
+++ lib/src/util/auto_dispose.dart
@@ -1,14 +1,56 @@
 import 'dart:async';
 
 /// Generic "disposable" to dispose/cancel/free some wrapped object.
-class Disposable {
-  final Function _disposable;
+abstract interface class Disposable {
+  /// Dispose the object wrapped by this disposable.
+  void dispose();
 
   /// Wrap some dispose call into this disposable for later disposition.
-  Disposable(this._disposable);
+  static wrapDispose(Function dispose) => _Disposable(dispose);
+}
 
-  /// Dispose the object wrapped by this disposable.
+/// Holds potentially multiple [Disposable]s to be [dispose]d in one call.
+/// Removes all [Disposable]s when [dispose] is called. Can be reused after
+/// [dispose] has been called.
+mixin CompositeDisposable implements Disposable {
+  final _disposables = <Disposable>[];
+
+  void addDisposable(Disposable disposable) => _disposables.add(disposable);
+
+  void wrapDispose(Function dispose) => _disposables.add(_wrap(dispose));
+
+  @override
+  void dispose() {
+    for (final it in _disposables) {
+      it.dispose();
+    }
+    _disposables.clear();
+  }
+}
+
+class _Disposable implements Disposable {
+  final Function _disposable;
+
+  _Disposable(this._disposable);
+
+  @override
   void dispose() => _disposable();
+}
+
+Disposable _wrap(something) {
+  final Disposable it;
+  if (something is Timer) {
+    it = _Disposable(() => something.cancel());
+  } else if (something is StreamController) {
+    it = _Disposable(() => something.close());
+  } else if (something is StreamSubscription) {
+    it = _Disposable(() => something.cancel());
+  } else if (something is Disposable) {
+    it = something;
+  } else {
+    throw ArgumentError("${something.runtimeType} not supported (yet)");
+  }
+  return it;
 }
 
 /// Auto-dispose system to manage [Disposable] instances and dispose all of them at once, or
@@ -36,19 +78,7 @@ mixin AutoDispose {
   /// has an unsupported type. In that case, wrap it into a [Disposable] before passing it to
   /// [autoDispose].
   void autoDispose(String tag, dynamic something) {
-    final Disposable it;
-    if (something is Timer) {
-      it = Disposable(() => something.cancel());
-    } else if (something is StreamController) {
-      it = Disposable(() => something.close());
-    } else if (something is StreamSubscription) {
-      it = Disposable(() => something.cancel());
-    } else if (something is Disposable) {
-      it = something;
-    } else {
-      throw ArgumentError("${something.runtimeType} not supported (yet)");
-    }
     _disposables.remove(tag)?.dispose();
-    _disposables[tag] = it;
+    _disposables[tag] = _wrap(something);
   }
 }
